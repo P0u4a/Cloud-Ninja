86bab48f99d2f933b2cb93157ab833aa67f6b980
P0u4a <66873325+P0u4a@users.noreply.github.com>
2022-04-26 21:48:22 +0930

    Update licenses.js

--- Cloud Ninja/licenses.js
+++ Cloud Ninja/licenses.js
@@ -18,7 +18,7 @@ class licenses extends Phaser.Scene {
         this.add.text(10, 120, "Richard Davey's profile: https://github.com/photonstorm", { fontFamily: "Arial", fontSize: 15, color: "#000000" });
         this.add.text(10, 140, "License (MIT): https://opensource.org/licenses/MIT", { fontFamily: "Arial", fontSize: 15, color: "#000000" });
         this.add.text(10,280, "---------------------------------------------------------------------------------------", { fontFamily: "Arial", fontSize: 15, color: "#000000" });
-        this.add.text(10, 310, "© 2020, Pouya Amiri. All rights reserved.", { fontFamily: "Calibri", fontSize: 20, color: "#000000" })
+        this.add.text(10, 310, "© 2020, PaulTheBuilder. All rights reserved.", { fontFamily: "Calibri", fontSize: 20, color: "#000000" })
         this.add.text(10, 330, "This software is licensed under the GNU General Public License v3.0.", { fontFamily: "Calibri", fontSize: 20, color: "#000000" })
         this.add.text(10, 350, "Link: https://opensource.org/licenses/gpl-3.0.html ", { fontFamily: "Calibri", fontSize: 20, color: "#000000" })
     
