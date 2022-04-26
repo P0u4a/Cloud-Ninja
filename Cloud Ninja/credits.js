468142252854cdcd0a7025aeb0c3eb1b8dab2d8a
P0u4a <66873325+P0u4a@users.noreply.github.com>
2022-04-26 21:41:30 +0930

    Update credits.js

--- Cloud Ninja/credits.js
+++ Cloud Ninja/credits.js
@@ -11,7 +11,7 @@ class credits extends Phaser.Scene {
         this.add.image(450,200, "background");
 
         this.add.text(100,100, "Thank you for playing Cloud Ninja", { fontFamily: "Arial", fontSize: 40, color: "#000066" })
-        this.add.text(290,300, "Created by Pouya Amiri", { fontFamily: "Arial", fontSize: 20, color: "#000066" })
+        this.add.text(290,300, "Created by PaulTheBuilder", { fontFamily: "Arial", fontSize: 20, color: "#000066" })
 
         this.cameras.main.fade(12000, 0, 0, 0, false, function(camera, progress) {
             if (progress > 0.9) {
